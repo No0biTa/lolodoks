c6d3cd45060e7212342aa5b78d7140a17bb87ee8
Akbar <firmanasya1601@gmail.com>
2023-04-11 01:46:14 +0700

    updated

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>The Lolodoks - Home</title>
+    </head>
+
+    <body>
+        <!-- Menu Seciton Start -->
+        <div class="menu">
+            Menu Here
+        </div>
+        <!-- Menu Section End -->
+
+        <!-- Main Content Section Start -->
+        <div class="menu">
+            Main Content Here
+        </div>
+        <!-- Main Content Seciton End -->
+
+        <!-- Footer Seciton Start -->
+        <div class="menu">
+            Footer Here
+        </div>
+        <!-- Footer Section End -->
+    </body>
+</html>
