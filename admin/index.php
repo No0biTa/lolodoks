b88506ba5b1efca5475f5e61d987ea052e87c084
Akbar <firmanasya1601@gmail.com>
2023-04-11 01:57:36 +0700

    updated

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,35 @@
 <html>
     <head>
         <title>The Lolodoks - Home</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+        <style>
+         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+        </style>
     </head>
 
     <body>
         <!-- Menu Seciton Start -->
         <div class="menu">
-            Menu Here
+            <div class="wrapper">
+                Menu Here
+            </div>
         </div>
         <!-- Menu Section End -->
 
         <!-- Main Content Section Start -->
         <div class="menu">
-            Main Content Here
+        <div class="wrapper">
+                Main Menu Here
+            </div>
         </div>
         <!-- Main Content Seciton End -->
 
         <!-- Footer Seciton Start -->
         <div class="menu">
-            Footer Here
+        <div class="wrapper">
+                Footer Here
+            </div>
         </div>
         <!-- Footer Section End -->
     </body>
