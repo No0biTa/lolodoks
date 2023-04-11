cfb5ec45ada418aafb6d7011f59dce4397e25408
Akbar <firmanasya1601@gmail.com>
2023-04-11 22:48:29 +0700

    updated 22:48

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,9 @@
         <title>The Lolodoks - Home</title>
 
         <link rel="stylesheet" href="../css/admin.css">
-        <style>
+        <!-- <style>
          @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
-        </style>
+        </style> -->
     </head>
 
     <body>
@@ -18,7 +18,7 @@
         <!-- Menu Section End -->
 
         <!-- Main Content Section Start -->
-        <div class="menu">
+        <div class="main-content">
         <div class="wrapper">
                 Main Menu Here
             </div>
@@ -26,9 +26,9 @@
         <!-- Main Content Seciton End -->
 
         <!-- Footer Seciton Start -->
-        <div class="menu">
+        <div class="footer">
         <div class="wrapper">
-                Footer Here
+                <p class="text-center">2023 All Rights Reserved, Lolodoks. Developed By - <a href="#"> Kelompok 6</a></p>
             </div>
         </div>
         <!-- Footer Section End -->
