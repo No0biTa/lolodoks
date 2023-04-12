d5235c7c1c88464dcc075adcb9f70da4d9027afa
Akbar <firmanasya1601@gmail.com>
2023-04-12 15:42:05 +0700

    Updated 12-04-2023

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,11 +3,59 @@
         <!-- Main Content Section Start -->
         <div class="main-content">
           <div class="wrapper">
-              <h1>Manage Admin</h1>
+              <h4>Manage Admin</h4>
+                <br />
+              <!-- Button to Add Admin -->
+              <!-- Compiled and minified CSS -->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+              <a href="#!" class="btn waves-effect waves-light">Add Admin</a>
+
+              <br /><br/>
+
+              <table class="tbl-full">
+                <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                </tr>
+
+                <tr>
+                        <td>1. </td>
+                        <td>Dummy</td>
+                        <td>Dummy</td>
+                        <td>
+                                Update Admin        
+                                Delete Admin
+                        </td>
+                </tr>
+
+                <tr>
+                        <td>2. </td>
+                        <td>Dummy</td>
+                        <td>Dummy</td>
+                        <td>
+                                Update Admin        
+                                Delete Admin
+                        </td>
+                </tr>
+
+                <tr>
+                        <td>3. </td>
+                        <td>Dummy</td>
+                        <td>Dummy</td>
+                        <td>
+                                Update Admin        
+                                Delete Admin
+                        </td>
+                </tr>
+              </table>
 
            </div>
         </div>
         <!-- Main Content Seciton End -->
 
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 <?php include('partials/footer.php'); ?>
