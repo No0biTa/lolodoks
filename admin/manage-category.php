b92a297ca972316a2665f32d1c1c850228d156a0
RizkiGiar <130471282+RizkiGiar@users.noreply.github.com>
2023-04-12 21:23:56 +0700

    Update manage-category.php

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -3,6 +3,52 @@
 <div class="main-content">
   <div class="wrapper">
     <h1>Manage Category</h1>
+    
+     <br />
+              <!-- Button to Add Admin -->
+              <a href="#"><button type="button" class="btn btn-default waves-effect waves-light">Add Category</button></a>
+
+              <br /><br/>
+
+              <table class="tbl-full">
+                <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                </tr>
+
+                <tr>
+                        <td>1. </td>
+                        <td>Dummy</td>
+                        <td>Dummy</td>
+                        <td>
+                                <a href="#" class="btn-secondary">Update Admin</a>        
+                                <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                </tr>
+
+                <tr>
+                        <td>2. </td>
+                        <td>Dummy</td>
+                        <td>Dummy</td>
+                        <td>
+                                <a href="#" class="btn-secondary">Update Admin</a>        
+                                <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                </tr>
+
+                <tr>
+                        <td>3. </td>
+                        <td>Dummy</td>
+                        <td>Dummy</td>
+                        <td>
+                                <a href="#" class="btn-secondary">Update Admin</a>        
+                                <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                </tr>
+              </table>
+
 </div>
 
 <?php include('partials/footer.php'); ?>
